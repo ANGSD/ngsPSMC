865d695b6d6de88f8432f0f25be191a15dbbc024
Thorfinn <thorfinn.sand@gmail.com>
2017-07-30 22:35:02 +0200

    added missing files

---
+++ compute.h
@@ -0,0 +1,21 @@
+double addProtect3(double a,double b, double c);
+double addProtect4(double a,double b, double c,double d);
+double addProtectN(double a[],int len);
+double addProtect2(double a,double b);
+void ComputeP11(unsigned numWin,int tk_l,double *P1,double *PP1,double **fw,double **bw,double *stationary,double *workspace);
+void ComputeP22(unsigned numWind,int tk_l,double **P,double *PP2,double **fw,double **bw,double *stationary);
+void ComputeP33(unsigned numWind,int tk_l,double *P3,double *PP3,double **fw,double **bw,double *stationary);
+void ComputeP44(unsigned numWind,int tk_l,double *P4,double *PP4,double **fw,double **bw,double *stationary,double *workspace);
+void ComputeP55(unsigned numWind,int tk_l,double **P,double *PP5,double **fw,double **bw,double *stationary);
+void ComputeP66(unsigned numWind,int tk_l,double **P,double *PP6,double **fw,double **bw,double *stationary);
+void ComputeP77(unsigned numWind,int tk_l,double **P,double *PP7,double **fw,double **bw,double *stationary);
+void ComputeP1(double *tk,int tk_l,double *P,double *epsize,double rho);
+void ComputeP5(double *tk,int tk_l,double *P,double *epsize);
+void ComputeP6(double *tk,int tk_l,double *P,double *epsize,double rho);
+void ComputeP2(int tk_l,double *P2,double *P5);
+void ComputeP3(double *tk,int tk_l,double *P3,double *epsize,double rho);
+void ComputeP4(double *tk,int tk_l,double *P4,double *epsize,double rho);
+void ComputeP7(double *tk,int tk_l,double *P7,double *P3,double *epsize,double rho);
+void ComputeP0(int tk_l,double *P0,double *P5); //probability P(T > i)
+double ComputeXXX(int i,int j,double **P);
+double calc_trans(int k, int j,double **P);
