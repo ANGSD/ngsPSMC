67e99173ceb621fa079948a5624390f889ddf824
Thorfinn <thorfinn.sand@gmail.com>
2019-05-09 18:04:52 +0200

    added hook for smc

--- main_psmc.cpp
+++ main_psmc.cpp
@@ -206,7 +206,7 @@ args * getArgs(int argc,char **argv,int dontprint){
   p->init_rho = 0.005367;
   p->init_theta = 0.000235;
   p->msstr = NULL;
-  
+  p->smc = 0;
   if(argc==0)
     return p;
 
@@ -222,8 +222,10 @@ args * getArgs(int argc,char **argv,int dontprint){
       p->RD = atoi(*(++argv));
     else  if(!strcasecmp(*argv,"-nThreads"))
       p->nThreads = atoi(*(++argv));
-   else  if(!strcasecmp(*argv,"-dospline"))
+    else  if(!strcasecmp(*argv,"-dospline"))
       p->dospline = atoi(*(++argv));
+    else  if(!strcasecmp(*argv,"-smc"))
+      p->smc = atoi(*(++argv));
     else  if(!strcasecmp(*argv,"-nIter"))
       p->nIter = atoi(*(++argv));
     else  if(!strcasecmp(*argv,"-p"))
@@ -266,7 +268,7 @@ args * getArgs(int argc,char **argv,int dontprint){
     p->seed = time(NULL);
   srand48(p->seed);
   
-  fprintf(stderr,"\t-> args: tole:%f maxiter:%d chr:%s start:%d stop:%d\n\t-> fname:\'%s\' seed:%ld winsize:%d RD:%d nThreads:%d doLinear:%d -nChr:%d -ms:\'%s\' -theta: %f -rho: %f -max_t:%f\n",p->tole,p->maxIter,p->chooseChr,p->start,p->stop,p->fname,p->seed,p->blocksize,p->RD,p->nThreads,p->doLinear,p->nChr,p->msstr,p->init_theta,p->init_rho,p->init_max_t);
+  fprintf(stderr,"\t-> args: tole:%f maxiter:%d chr:%s start:%d stop:%d\n\t-> fname:\'%s\' seed:%ld winsize:%d RD:%d nThreads:%d doLinear:%d -nChr:%d -ms:\'%s\' -theta: %f -rho: %f -max_t:%f -smc:%d\n",p->tole,p->maxIter,p->chooseChr,p->start,p->stop,p->fname,p->seed,p->blocksize,p->RD,p->nThreads,p->doLinear,p->nChr,p->msstr,p->init_theta,p->init_rho,p->init_max_t,p->smc);
   extern int doQuadratic;
   if(p->doLinear==0)
     doQuadratic=1;
